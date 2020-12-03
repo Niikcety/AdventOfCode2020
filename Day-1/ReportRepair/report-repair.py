bfe64ad8183f240e388f23155bef2425cef8e80d
Nikolay <nikolaineshev1999@gmail.com>
2020-12-03 12:23:30 +0200

    added serializer function to day 1 solution

--- Day-1/ReportRepair/report-repair.py
+++ Day-1/ReportRepair/report-repair.py
@@ -1,13 +1,19 @@
-from numbers import numbers
 
-def part_one():
+def serializer():
+    numbers = []
+    file = open("numbers.txt", "r")
+    for x in file:
+        numbers.append(int(x))
+    return numbers
+
+def part_one(numbers):
     for numb1 in numbers:
         for numb2 in numbers:
             if numb1 + numb2 == 2020:
                 return numb1 * numb2
 
 
-def part_two():
+def part_two(numbers):
     for numb1 in numbers:
         for numb2 in numbers:
             for numb3 in numbers:
@@ -15,4 +21,6 @@ def part_two():
                     return numb1 * numb2 * numb3
 
 if __name__ == '__main__':
-    print(part_two())
+    numbers = serializer()
+    print(part_one(numbers))
+    print(part_two(numbers))
